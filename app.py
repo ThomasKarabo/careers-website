4efbdd8a6abca53d1ecc3097f62e0484355eee87
Thomas Karabo Mohlapo <79021656+ThomasKarabo@users.noreply.github.com>
2025-07-24 10:53:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,44 +4,6 @@ import sqlite3
 
 app = Flask(__name__)
 
-'''JOBS = [
-    {
-        'id':1,
-        'title': 'Junior Data Analyst',
-        'experience': '1-2 years',
-        'location': 'Johannesburg, Gauteng',
-        'salary': 'R35,000.00'
-    },
-    {
-        'id':2,
-        'title': 'Senior Data Analyst',
-        'experience': '3-5 years',
-        'location': 'Cape Town, Western Cape',
-        'salary': 'R55,000.00'
-    },
-    {
-        'id':3,
-        'title': 'Data Scientist',
-        'experience': '5+ years',
-        'location': 'Durban, KwaZulu-Natal',
-        'salary': 'R75,000.00'
-    },
-    {
-        'id':4,
-        'title': 'Machine Learning Engineer',
-        'experience': '3-5 years',
-        'location': 'Pretoria, Gauteng',
-        'salary': 'R85,000.00'
-    },
-    {
-        'id':5,
-        'title': 'Data Engineer',
-        'experience': '2-4 years',
-        'location': 'Port Elizabeth, Eastern Cape',
-        'salary': 'R65,000.00',
-    }
-]
-'''
 def load_jobs_from_db():
     connection = sqlite3.connect('careers_db.db')
     cursor = connection.cursor()
